2d612f2cebd91b39bb7a05d0693dfe9d39e79e85
Leonardo Bucioli Simioni <lsimioni@ciandt.com>
2021-03-18 15:45:54 -0300

    Adição de autenticação

---
+++ src/main/java/br/com/alura/forum/model/Perfil.java
@@ -0,0 +1,24 @@
+package br.com.alura.forum.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Perfil implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Override
+    public String getAuthority() {
+        return this.nome;
+    }
+}
